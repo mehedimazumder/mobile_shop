0bd1e8fbb042d133967c2e14a8f82c9d7112ac7e
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-28 02:01:33 +0600

    1.all components and navbar created

---
+++ tech-shop/src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Product from './Product';
+
+export default class   extends Component {
+    render() {
+        return (
+            <div>
+                <Product />
+            </div>
+        )
+    }
+}
