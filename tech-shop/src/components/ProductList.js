2490c7327a51315a7d3693ce64fcc424ce68641e
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-28 13:41:02 +0600

    styled-components and title

--- tech-shop/src/components/ProductList.js
+++ tech-shop/src/components/ProductList.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import Product from './Product';
-
+import Title from './Title';
+import {storeProducts} from '../data';
 export default class   extends Component {
+    state={
+        Products: storeProducts
+    }
     render() {
-        return (
-            <div>
-                <Product />
-            </div>
+        console.log(this.state.Products);
+        return ( 
+           <React.Fragment>
+               <div className="py-5">
+                   <div className="container">
+                        <Title name="our" title="products" />
+                       <div className="row">
+
+                       </div>
+                   </div>
+               </div>
+           </React.Fragment>
         )
     }
 }
