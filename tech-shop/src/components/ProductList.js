633203c7a308dfa2b419142fa4ac7325b4cc8a28
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-28 18:35:35 +0600

    product list added

--- tech-shop/src/components/ProductList.js
+++ tech-shop/src/components/ProductList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
-
+import {ProductConsumer } from '../context';
 
 export default class   extends Component {
     state={
@@ -16,7 +16,13 @@ export default class   extends Component {
                    <div className="container">
                         <Title name="our" title="products" />
                        <div className="row">
-
+                            <ProductConsumer>
+                                {output => {
+                                return output.products.map(product => {
+                                    return <Product key={product.id} product={product} />
+                                });
+                                }}
+                            </ProductConsumer>
                        </div>
                    </div>
                </div>
--- tech-shop/src/context.js
+++ tech-shop/src/context.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import { storeProducts, detailProduct } from './data';
 
 const ProductContext = React.createContext();
 //provider
 //consumer
 
 class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detaiProduct: detailProduct
+    };
+    handleDetaile = () => {
+        console.log("details P");
+    };
+
+    addToCart = () => {
+        console.log("cart add");
+    };
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetaile: this.state.handleDetaile,
+                addToCart: this.state.addToCart
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         )
