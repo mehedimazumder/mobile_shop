b8454c973f53876fe32d982f3c38425206f221bc
mehedi <mehedirumc12cuet14@gmail.com>
2019-12-05 15:01:21 +0600

    cart component added

--- tech-shop/src/context.js
+++ tech-shop/src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: [],
+        cart: [...storeProducts],
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubtotal:0,
+        cartTax:0,
+        cartTotal:0
     };
     componentDidMount(){
         this.setProducts();
@@ -67,6 +70,22 @@ class ProductProvider extends Component {
         })
     }
 
+    increment = id => {
+        console.log('This is increment')
+    }
+
+    decrement = id => {
+        console.log('This is decrement')
+    }
+
+    removeItem = id => {
+        console.log('item removed')
+    }
+
+    clearCart = () => {
+        console.log('cart was cleared')
+    }
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -74,7 +93,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal:this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem:this.removeItem,
+                clearCart:this.clearCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
