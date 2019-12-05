6515850b3dc238fd6f8bd3e7043ed877bafeef94
mehedi <mehedirumc12cuet14@gmail.com>
2019-12-05 16:39:32 +0600

    increment item method

--- tech-shop/src/context.js
+++ tech-shop/src/context.js
@@ -71,7 +71,20 @@ class ProductProvider extends Component {
     }
 
     increment = id => {
-        console.log('This is increment')
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(() => {
+            return {cart: [...tempCart]}
+        }, () => {
+            this.addTotals()
+        })
     }
 
     decrement = id => {
